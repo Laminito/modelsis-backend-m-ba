80a05075bd374128e70c7bac9c42951f0006d913
Laminito <mba25116@gmail.com>
2024-01-06 23:13:47 +0000

    amelioration des reponses

--- src/main/java/sn/modelsisbackendmba/controller/ProductTypeController.java
+++ src/main/java/sn/modelsisbackendmba/controller/ProductTypeController.java
@@ -56,24 +56,12 @@ public class ProductTypeController {
             resultMap = new HashMap<>();
             resultMap.put("result", errorResponse);
             responseDTO.setModelsis(resultMap);
-
             log.error("Erreur lors de la récupération des types de produits : {}", ex.toString());
         }
-        log.info("Récupération de tous les types de produits réussie");
+        log.info("Récupération de tous les types de produits à réussie avec succée");
         return responseDTO;
     }
 
-
-  /*  @PostMapping
-    public ResponseEntity<ProductType> addProductType(@RequestBody ProductType productType) {
-        try {
-            ProductType newProductType = productTypeService.addProductType(productType);
-            return ResponseEntity.ok(newProductType);
-        } catch (IllegalArgumentException ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-    }
-*/
     @PutMapping
     public ResponseEntity<ProductTypeDto> updateProductType(@RequestBody ProductType product) {
         try {
@@ -82,65 +70,84 @@ public class ProductTypeController {
             productDto.setType(updatedProduct.getType());
             productDto.setIdTypeProduct(updatedProduct.getId());
 
-            log.info("Le Type de Produit a ete mis à jour avec succès : {}", updatedProduct.getId());
+            log.info("Le Type de Produit {} a ete mis à jour avec succès : {}",updatedProduct.getType(), updatedProduct.getId());
             return ResponseEntity.ok(productDto);
         } catch (Exception ex) {
             log.error("Erreur lors de la mise à jour du type de produit : {}", ex.toString());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
- /*   @GetMapping
-    public List<ProductType> getAllProductTypes() {
-        try {
-            return productTypeService.findAllProductTypes();
-        } catch (Exception ex) {
-            System.out.println("Une erreur est survenue lors de la récupération des types de produits : " + ex.toString());
-            return null;
-        }
-    }*/
-/*    @DeleteMapping("/{productTypeId}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable String productTypeId) {
-        try {
-            productTypeService.deleteProductType(productTypeId);
-            log.info("Le Type de Produit est supprimé avec succès : {}", productTypeId);
-            return ResponseEntity.noContent().build();
-        } catch (Exception ex) {
-            log.error("Erreur lors de la suppression du type de produit : {}", ex.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }*/
- @DeleteMapping("/{productTypeId}")
- public ModelsIsResponseDTO deleteProduct(@PathVariable String productTypeId) {
-     ProductType result = null;
-     ModelsIsResponseDTO responseDTO = new ModelsIsResponseDTO();
-     try {
-         result = productTypeService.deleteProductType(productTypeId);
-         CustomResponse customResponse = ResponseFactory.createCustomResponse(
-                 Constants.STATUS_MESSAGE_SUCCESS_BODY,
-                 Constants.STATUS_VALUE_OK,
-                 "Le Type de Produit est supprimé avec succès",
-                 null
-         );
 
-         Map<String, CustomResponse> resultMap = new HashMap<>();
-         resultMap.put("result", customResponse);
-         responseDTO.setModelsis(resultMap);
-     } catch (Exception ex) {
-         CustomResponse errorResponse = ResponseFactory.createCustomResponse(
-                 Constants.STATUS_MESSAGE_NOT_FOUND_BODY,
-                 Constants.STATUS_VALUE_BAD_REQUEST,
-                 "Erreur lors de la suppression du type de produit : " + ex.getMessage(),
-                 null
-         );
+    @DeleteMapping("/{productTypeId}")
+    public ModelsIsResponseDTO deleteProduct(@PathVariable String productTypeId) {
+         ProductType result = null;
+         ModelsIsResponseDTO responseDTO = new ModelsIsResponseDTO();
+         try {
+             result = productTypeService.deleteProductType(productTypeId);
+             CustomResponse customResponse = ResponseFactory.createCustomResponse(
+                     Constants.STATUS_MESSAGE_SUCCESS_BODY,
+                     Constants.STATUS_VALUE_OK,
+                     "Le Type de Produit est supprimé avec succès",
+                     null
+             );
 
-         Map<String, CustomResponse> resultMap = new HashMap<>();
-         resultMap.put("result", errorResponse);
-         responseDTO.setModelsis(resultMap);
+             resultMap = new HashMap<>();
+             resultMap.put("result", customResponse);
+             responseDTO.setModelsis(resultMap);
+         } catch (Exception ex) {
+             CustomResponse errorResponse = ResponseFactory.createCustomResponse(
+                     Constants.STATUS_MESSAGE_NOT_FOUND_BODY,
+                     Constants.STATUS_VALUE_BAD_REQUEST,
+                     "Erreur lors de la suppression du type de produit : " + ex.getMessage(),
+                     null
+             );
 
-         log.error("Erreur lors de la suppression du type de produit : {}", ex.getMessage());
+             Map<String, CustomResponse> resultMap = new HashMap<>();
+             resultMap.put("result", errorResponse);
+             responseDTO.setModelsis(resultMap);
+
+             log.error("Erreur lors de la suppression du type de produit : {}", ex.getMessage());
+         }
+         log.info("Le Type de Produit {} est supprimé avec succès : {}",result.getType(), productTypeId);
+         return responseDTO;
      }
-     log.info("Le Type de Produit {} est supprimé avec succès : {}",result.getType(), productTypeId);
-     return responseDTO;
- }
+
+    @PostMapping
+    public ResponseEntity<ModelsIsResponseDTO> addProductType(@RequestBody ProductType productType) {
+        try {
+            ProductType newProductType = productTypeService.addProductType(productType);
+
+            CustomResponse customResponse = ResponseFactory.createCustomResponse(
+                    Constants.STATUS_MESSAGE_SUCCESS_BODY,
+                    Constants.STATUS_VALUE_OK,
+                    "Nouveau type de produit ajouté avec succès",
+                    newProductType
+            );
+
+            Map<String, CustomResponse> resultMap = new HashMap<>();
+            resultMap.put("modelsis", customResponse);
+
+            ModelsIsResponseDTO responseDTO = new ModelsIsResponseDTO();
+            responseDTO.setModelsis(resultMap);
+
+            return ResponseEntity.ok(responseDTO);
+        } catch (IllegalArgumentException ex) {
+            CustomResponse errorResponse = ResponseFactory.createCustomResponse(
+                    Constants.STATUS_MESSAGE_BAD_REQUEST_BODY,
+                    Constants.STATUS_VALUE_BAD_REQUEST,
+                    "Erreur lors de l'ajout du type de produit : " + ex.getMessage(),
+                    null
+            );
+
+            Map<String, CustomResponse> resultMap = new HashMap<>();
+            resultMap.put("modelsis", errorResponse);
+
+            ModelsIsResponseDTO responseDTO = new ModelsIsResponseDTO();
+            responseDTO.setModelsis(resultMap);
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDTO);
+        }
+    }
+
 
 }
